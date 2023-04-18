8f38977f0c1b34d9f563ba3125786142ad258f5b
yanwuzhang <yanwuzhang@tencent.com>
2023-04-18 23:52:16 +0800

    增加测试Module

---
+++ Source/Test/Private/Test.cpp
@@ -0,0 +1,17 @@
+﻿#include "Test.h"
+
+#define LOCTEXT_NAMESPACE "FTestModule"
+
+void FTestModule::StartupModule()
+{
+    
+}
+
+void FTestModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FTestModule, Test)
---
+++ Source/Test/Test.Build.cs
@@ -0,0 +1,26 @@
+﻿using UnrealBuildTool;
+
+public class Test : ModuleRules
+{
+    public Test(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
+            }
+        );
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "CoreUObject",
+                "Engine",
+                "Slate",
+                "SlateCore"
+            }
+        );
+    }
+}
